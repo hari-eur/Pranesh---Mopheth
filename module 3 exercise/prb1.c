9f5288af89970dd91ab0e0fbdc8c980fede32821
leopranesh <praneshkumarsece2019@citchennai.net>
2023-03-18 14:33:26 +0530

    updated code

--- module 3 exercise/prb1.c
+++ module 3 exercise/prb1.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+# define print(...) printf(__VA_ARGS__)
 int main(){
-    printf("heloo world");
+    char str[25],str1[25],str2[25];
+    scanf("%s",str);
+    scanf("%s",str1);
+    scanf("%s",str2);
+    print("welcome to %s\n",str);
+    print("hello to %s :Welcome to %s\n",str1,str2);
     return 0;
 }
